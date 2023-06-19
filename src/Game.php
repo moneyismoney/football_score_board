cc40b770f05629a708d6f75b82975ffc3cd962c5
oleksandr.tymchenko <moneyismoney@gmail.com>
2023-06-19 04:16:50 +0300

    draft of the class and test

---
+++ src/Game.php
@@ -0,0 +1,37 @@
+<?php
+
+class Game
+{
+    private $id;
+    public $homeTeam;
+    public $awayTeam;
+    public $homeTeamScore;
+    public $awayTeamScore;
+
+    private function getGameById() {
+        pass;
+    }
+
+    private function getGameByHomeAndAway() {
+        pass;
+    }
+
+    private function updateHomeScore($gameId, $newHomeScore) {
+        pass;
+    }
+
+    private function updateAwayScore($gameId, $newAwayScore) {
+        pass;
+    }
+
+    private function startGame($homeTeam, $awayTeam) {
+        pass;
+    }
+
+    private function finishGame($id) {
+        pass;
+    }
+
+
+
+}
